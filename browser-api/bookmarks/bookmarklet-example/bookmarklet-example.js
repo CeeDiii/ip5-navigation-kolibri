b8d092b503b4229cfd0c0c66dc6103f11ddf7987
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-05-21 13:31:29 +0200

    Refactor

---
+++ browser-api/bookmarks/bookmarklet-example/bookmarklet-example.js
@@ -0,0 +1,4 @@
+function changeContent() {
+    content = document.getElementById("content");
+    content.innerText = "This is my brand new content after using the bookmark";
+}
---
+++ browser-api/bookmarks/bookmarklet-example/usage.md
@@ -0,0 +1,3 @@
+# How to use the bookmarklet functionality
+
+Drag and drop the href to your bookmark list and execute javascript functions directly from a bookmark
